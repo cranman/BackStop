79d62846b9067912f51407bbff3ce1eb20e99a37
Matthew Cranham <cranman@hotmail.co.uk>
2012-09-12 14:29:49 +0100

    rewrote backstop to work on google pages

--- content.js
+++ content.js
@@ -1,3 +1,21 @@
-$(document).bind('keydown', {combi:'backspace', disableInInput: true}, function() {
-	return false;
+$(document).keydown(function(e) {
+	// If backspace is pressed
+	if (e.which === 8) {
+		var active = $(document.activeElement);
+		
+		notEditable = function(element) {
+			var edit = element.attr('contenteditable');
+			
+			// Ensure backspace still works on any element with contenteditable="true"
+			if (typeof edit !== 'undefined' && edit !== false) {
+				return true;
+			} else if (element.is('input, textarea')) {
+				return true;
+			}
+			
+			return false;
+		}
+		
+		return notEditable(active);
+	}
 });
